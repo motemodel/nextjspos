3a78d928a16957acd86f0bb3343177a917b30dbf
voravit euavatanakorn <motemodel@gmail.com>
2025-12-05 19:07:44 +0700

    include component hero

---
+++ src/app/(front)/product/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import Features from "@/components/features";
+
+export default function Product() {
+    return (
+        <div>
+            <Features />
+        </div>
+    );
+}
